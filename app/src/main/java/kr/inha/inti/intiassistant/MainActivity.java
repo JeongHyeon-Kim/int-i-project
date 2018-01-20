827b84d12d5e1a1f9d353dc0860e81897f67a3ff
PureBlack <wjdgus1993@inha.edu>
2018-01-21 00:32:11 +0900

    version0.0.1

--- app/src/main/java/kr/inha/inti/intiassistant/MainActivity.java
+++ app/src/main/java/kr/inha/inti/intiassistant/MainActivity.java
@@ -1,8 +1,10 @@
 package kr.inha.inti.intiassistant;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,6 +37,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(recognitionListener);
+
+        textView2 = (TextView) findViewById(R.id.result);
+        textView = (TextView) findViewById(R.id.voiceText);
+        //URL 설정
+        //String url = "http://39.115.148.109:5000/";
+        String url = "http://39.115.148.109:5000/request/google-assistant";
+
+        //AsyncTask를 통해 HttpURLConnection 수행
+        downloadTask processingThread = new downloadTask(url, null);//AsyncTask 객체 생성
+        processingThread.execute(); //일반적, 여러 AsyncTask 객체를 만들어 다수의 작업 수행, execute()가 호출된 순서대로 처리
+        //processingThread.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, arr); //병렬처리를 위한 수행법, 여러 AsyncTask 객체를 만들어 다수의 작업 수행, 순서에 상관없이 동시처리
+
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -48,15 +69,6 @@ public class MainActivity extends AppCompatActivity {
             }
         } // if 끝
 
-        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
-
-        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        mRecognizer.setRecognitionListener(recognitionListener);
-
-        textView2 = (TextView) findViewById(R.id.result);
-        textView = (TextView) findViewById(R.id.voiceText);
 
         Button button = (Button) findViewById(R.id.voice);
         button.setOnClickListener(new View.OnClickListener() {
@@ -115,10 +127,10 @@ public class MainActivity extends AppCompatActivity {
             ArrayList<String> mResult = bundle.getStringArrayList(key);
 
             String[] rs = new String[mResult.size()];
-            mResult.toArray(rs);
+            mResult.toArray(rs);//rs에 텍스트 데이터 받아옴
 
             textView.setText(rs[0]);
-            sendObject(rs[0]);
+            sendObject(rs[0]); //JSONObject로 보내기 위해 호출
         }
 
         @Override
@@ -132,22 +144,64 @@ public class MainActivity extends AppCompatActivity {
 
     private void sendObject(String s){
         JSONObject jsonObject = new JSONObject();
+        //JSONArray jsonArray = new JSONArray(); //이런 형식도 존재함
         try{
-            jsonObject.put("textVoice", s);
-            textView2.setText(jsonObject.getString("textVoice"));
+            jsonObject.put("textVoice", s);//textVoice가 key, s가 value
+            //textView2.setText(jsonObject.getString("textVoice"));//이건 그 내용 출력
         }catch (JSONException e){
             e.printStackTrace();
         }
        // receiveObject(jsonObject);
     }
 
-    private void receiveObject(JSONObject data){
+    private void receiveObject(JSONObject data){ //JSONObject를 받아오는 것
        // recyclerView.setVisibility(View.GONE);
         //objectResultLo.setVisibility(View.VISIBLE);
         try{
-            textView.setText("reponse : " + data.getString("reponse"));
+            textView.setText("reponse : " + data.getString("reponse")); //받아와서 key는 response로 지정하고 getString으로 읽음
         } catch (JSONException e){
             e.printStackTrace();
         }
+    }
+
+    private class downloadTask extends AsyncTask<Void, Void, String>{
+
+        private String url;
+        private ContentValues values;
+
+        public downloadTask(String url, ContentValues values){
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result;
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values);
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView2.setText(s);//이건 그 내용 출력
+        }
+
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected void onCancelled(String result) {
+//            super.onCancelled(result);
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Void... values) {
+//            super.onProgressUpdate(values);
+//        }
     }
 }
